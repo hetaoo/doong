1227c3ab970a4fb439e7c69b23f0a6b621a0b099
hetaoo <i@hetaoo.cn>
2017-10-12 00:20:18 +0800

    修改query类

--- doong-common/src/main/java/cn/hetaoo/doong/common/utils/Query.java
+++ doong-common/src/main/java/cn/hetaoo/doong/common/utils/Query.java
@@ -28,10 +28,10 @@ public class Query extends LinkedHashMap<String, Object> {
         this.put("pageSize", pageSize);
 
         //防止SQL注入（因为sidx、order是通过拼接SQL实现排序的，会有SQL注入风险）
-        String sidx = params.get("sidx").toString();
-        String order = params.get("order").toString();
-        this.put("sidx", SQLFilter.sqlInject(sidx));
-        this.put("order", SQLFilter.sqlInject(order));
+//        String sidx = params.get("sidx").toString();
+//        String order = params.get("order").toString();
+//        this.put("sidx", SQLFilter.sqlInject(sidx));
+//        this.put("order", SQLFilter.sqlInject(order));
     }
 
 
